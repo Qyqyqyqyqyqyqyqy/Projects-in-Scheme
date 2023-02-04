93310fe7949db3f03faba8786889b547b000c61f
Baihaqy Aufa Ghiffari <113879684+Qyqyqyqyqyqyqyqy@users.noreply.github.com>
2023-02-05 00:25:04 +0700

    Update Project_1.rkt

--- Project_1.rkt
+++ Project_1.rkt
@@ -1,10 +1,10 @@
 #lang scheme
 
 
+(define (square x)
+  (* x x))
+
 (define pi 3.14159265358979323846264338327950288419716939937510)
 (define radius 7)
 (define area (* pi (expt radius 2)))
-
-(define (square x)
-  (* x x))
 
