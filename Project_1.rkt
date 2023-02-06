18644ca62f80b7785b3b165a53ccfc9f77bc8af8
Baihaqy Aufa Ghiffari <113879684+Qyqyqyqyqyqyqyqy@users.noreply.github.com>
2023-02-06 07:04:42 +0700

    Add files via upload

--- Project_1.rkt
+++ Project_1.rkt
@@ -1,10 +1,20 @@
 #lang scheme
 
-
 (define (square x)
   (* x x))
 
+
+(display "Please enter the radius of the circle: ")
+(define radius (read))
 (define pi 3.14159265358979323846264338327950288419716939937510)
-(define radius 7)
-(define area (* pi (expt radius 2)))
+(define area (* pi (square radius)))
+
+
+(define (sum-of-squares x y) (+ (square x) (square y)))
+
+
+(define (abs x)
+  (cond((> x 0) x)
+       ((= x 0) 0)
+       ((< x 0)(- x))))
 
